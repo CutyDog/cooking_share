640f1ee54d5e0f91bc6cef056e977c5292551f27
EC2 Default User <ec2-user@ip-172-31-41-232.ap-northeast-1.compute.internal>
2021-02-16 17:16:14 +0000

    Postモデル、コントローラーの作成

--- Gemfile
+++ Gemfile
@@ -7,6 +7,12 @@ ruby '2.6.3'
 gem 'rails', '~> 6.1.2', '>= 6.1.2.1'
 # Use sqlite3 as the database for Active Record
 gem 'sqlite3', '~> 1.4'
+
+gem 'devise'
+
+gem 'carrierwave'
+#gem 'rmagick'
+
 # Use Puma as the app server
 gem 'puma', '~> 5.0'
 # Use SCSS for stylesheets
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,9 @@ Rails.application.routes.draw do
   get '/post',   to: 'home#index'
   get '/mypage', to: 'home#index'
   get '/search', to: 'home#index'
+  
+  namespace :api, format: 'json' do
+    resources :posts, only: [:index, :create, :update]
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
